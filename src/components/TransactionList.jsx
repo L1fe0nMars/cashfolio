6229aad91e6533d66f24d6c6ef29b729ee31ea69
L1fe0nMars <jkama005@gmail.com>
2024-04-19 00:19:18 -0400

    Added title to add transaction button, tweaked info alignment

--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -76,7 +76,7 @@ const TransactionList = () => {
                         </select>
                     </form>
 
-                    <button className="add-transaction-btn" onClick={() => toggleNewTransaction(true)} aria-label="Add transaction">
+                    <button className="add-transaction-btn" onClick={() => toggleNewTransaction(true)} title="Add transaction" aria-label="Add transaction">
                         <svg xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" width="24" height="24">
                             <path d="M17,11H13V7a1,1,0,0,0-1-1h0a1,1,0,0,0-1,1v4H7a1,1,0,0,0-1,1H6a1,1,0,0,0,1,1h4v4a1,1,0,0,0,1,1h0a1,1,0,0,0,1-1V13h4a1,1,0,0,0,1-1h0A1,1,0,0,0,17,11Z"/>
                         </svg>
